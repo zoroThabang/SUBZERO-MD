57a47e5050477f8f6f28b6b4b69cd3427688725b
Mr Frank OFC <132457111+mrfrankofcc@users.noreply.github.com>
2025-05-14 13:50:23 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
-
 module.exports = {
+//=====================================
     // ONLY EDIT SETTINGS BELOW
     
     // ===== BOT CORE SETTINGS =====

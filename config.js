18facf949464d06f811534c9a90b28caf1ade1af
Mr Frank OFC <132457111+mrfrankofcc@users.noreply.github.com>
2025-06-07 09:43:16 +0200

    Update config.js

--- config.js
+++ config.js
@@ -51,7 +51,7 @@ module.exports = {
     ANTI_LINK: process.env.ANTI_LINK || "true",                 // Block links in groups?
     ANTI_VV: process.env.ANTI_VV || "true",                     // Block view-once messages?
     DELETE_LINKS: process.env.DELETE_LINKS || "true",          // Auto-delete links?
-    ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log",          // Log deleted messages (or 'same' to resend)
+    ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox",          // Log deleted messages (or 'same' to resend)
     ANTI_BOT: process.env.ANTI_BOT || "true",
     PM_BLOCKER: process.env.PM_BLOCKER || "true",
 

dbfd026980dc63a1c1b35d50bda0aafa43f64747
Mr Frank OFC <132457111+mrfrankofcc@users.noreply.github.com>
2025-07-17 16:12:45 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,7 @@
 module.exports = {
-  SESSION_ID: " ", // PUT SESSION ID HERE 
-  OWNER_NUMBER: "263719647303", // PUT OWNER NUMBER HERE
-  PREFIX: "." // DEFAULT PREFIX
+  SESSION_ID: "", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
+
+// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )

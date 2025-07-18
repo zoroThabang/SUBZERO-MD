7117bd64641d7bded42d56236ebb4855e7a7fbd7
Zoro <nhlapothabang234@gmail.com>
2025-07-18 02:07:45 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "", // Put Session Id Here Inside "ABCD1234"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
